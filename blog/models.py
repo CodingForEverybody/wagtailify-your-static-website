e62b36d9777300358c72f9c7718f7e563a0a1357
Kalob Taulien <kalob.taulien@gmail.com>
2024-04-02 12:17:30 -0600

    Orderables and Snippets

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,51 @@
 from django.db import models
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, InlinePanel
 from wagtail.fields import StreamField
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.snippets.models import register_snippet
+
+from modelcluster.fields import ParentalKey
+
+@register_snippet
+class BlogCategory(models.Model):
+    name = models.CharField(max_length=255)
+    color = models.CharField(choices=[
+        ('blue', 'Blue'),
+        ('green', 'Green'),
+        ('yellow', 'Yellow'),
+        ('grey', 'Grey'),
+    ], help_text='Background color', default='grey', max_length=6)
+
+    @property
+    def text_color(self):
+        if self.color == 'grey':
+            return 'blue-dark'
+        elif self.color == 'yellow':
+            return 'yellow-dark'
+        return self.color
+
+
+    class Meta:
+        verbose_name_plural = 'blog categories'
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPageCategories(Orderable):
+    page = ParentalKey('blog.BlogPage', related_name='categories')
+    category = models.ForeignKey(
+        'blog.BlogCategory',
+        on_delete=models.CASCADE,
+    )
+
+    panels = [
+        FieldPanel('category'),
+    ]
+
 
 class BlogIndexPage(Page):
     max_count = 1
@@ -48,6 +89,7 @@ class BlogPage(Page):
     ])
 
     content_panels = Page.content_panels + [
+        InlinePanel('categories', label='Categories'),
         FieldPanel('reading_time_in_minutes'),
         FieldPanel('body'),
     ]
