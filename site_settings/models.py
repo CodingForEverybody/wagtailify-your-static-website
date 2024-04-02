4f52719575a848b9b8791a97d66fb590e0d1af74
Kalob Taulien <kalob.taulien@gmail.com>
2024-04-02 10:03:17 -0600

    Custom Logo and Site Name

--- site_settings/models.py
+++ site_settings/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from wagtail.contrib.settings.models import BaseGenericSetting, register_setting
 from wagtail.admin.panels import FieldPanel
 
+
 @register_setting
 class FooterLinks(BaseGenericSetting):
     github = models.URLField(blank=True, null=True)
@@ -11,4 +12,19 @@ class FooterLinks(BaseGenericSetting):
     panels = [
         FieldPanel("github"),
         FieldPanel("linkedin"),
+    ]
+
+
+@register_setting
+class BrandSettings(BaseGenericSetting):
+    logo = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        FieldPanel("logo"),
     ]
