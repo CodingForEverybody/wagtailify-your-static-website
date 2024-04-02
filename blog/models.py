903ba51e5831c238f2c9c92aeb8fd3a987ae5248
Kalob Taulien <kalob.taulien@gmail.com>
2024-04-02 11:57:48 -0600

    StreamFields

--- blog/models.py
+++ blog/models.py
@@ -2,11 +2,13 @@ from django.db import models
 from wagtail.models import Page
 
 from wagtail.admin.panels import FieldPanel
-
+from wagtail.fields import StreamField
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
 
 class BlogIndexPage(Page):
     max_count = 1
-    # subpage_types = ['blog.BlogPage'] # TODO
+    subpage_types = ['blog.BlogPage']
     parent_page_types = ['home.HomePage']
 
     summary = models.TextField(blank=True, max_length=500)
@@ -22,3 +24,30 @@ class BlogIndexPage(Page):
         # TODO:
         # context['posts'] = BlogPage.objects.live().public().order_by('-first_pubished_at')
         return context
+
+
+class BlogPage(Page):
+    parent_page_types = ['blog.BlogIndexPage']
+
+    reading_time_in_minutes = models.IntegerField()
+    body = StreamField([
+        ('content', blocks.RichTextBlock(
+            features=['bold', 'italic', 'link', 'ol', 'ul', 'hr'],
+            template='blocks/richtext.html'
+        )),
+        ('image', ImageChooserBlock(
+            template='blocks/image.html',
+        )),
+        ('quote', blocks.BlockQuoteBlock(
+            template='blocks/quote.html',
+        )),
+        ('twitter_block', blocks.StructBlock([
+            ('text', blocks.CharBlock()),
+            ('author', blocks.CharBlock()),
+        ], template='blocks/twitter_block.html'))
+    ])
+
+    content_panels = Page.content_panels + [
+        FieldPanel('reading_time_in_minutes'),
+        FieldPanel('body'),
+    ]
